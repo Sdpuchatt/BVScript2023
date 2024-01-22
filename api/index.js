069f60ee7043b0f1486517f128ba090bc7954c98
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2024-01-22 20:10:31 -0300

    actualizar borrar en mongo

--- api/index.js
+++ api/index.js
@@ -8,8 +8,39 @@ const User = mongoose.model('User',{
 })
 
 const crear = async () => {
-    const user = new User({username: 'chanchito feliz', edad: 15})
+    const user = new User({username: 'chanchito trizte', edad: 25})
     const savedUser = await user.save()
     console.log(savedUser)
 }
-crear()
+//crear()
+const buscarTodo = async () => {
+    const users = await User.find()
+    console.log(users)
+}
+//buscarTodo()
+const buscar = async () => {
+    const user = await User.find({ username: 'chanchito trizte'})
+    console.log(user)
+}
+// buscar()
+const buscarUno = async () => {
+    const user = await User.findOne({username:'chanchito feliz'})
+    console.log(user)
+}
+// buscarUno()
+const actualizar = async () =>{
+    const user = await User.findOne({username : 'chanchito trizte'})
+    console.log(user)
+    user.edad = 45
+    await user.save()
+    console.log(user)
+}
+// actualizar();
+const eliminar = async () => {
+    const user = await User.findOne({username : 'chanchito trizte'})
+    console.log(user)
+    if(user){
+        await user.remove()
+    }
+}
+eliminar()
